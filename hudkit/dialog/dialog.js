563d81078d42047b05f1353ce4483bebc7ad8f1c
Jason Frame <jason@onehackoranother.com>
2013-03-13 16:33:13 +0000

    Added dialog

---
+++ hudkit/dialog/dialog.js
@@ -0,0 +1,317 @@
+;(function(global, hk) {
+  
+  var SHOW_HIDDEN         = 1,
+      SHOW_VISIBLE        = 2,
+      SHOW_ANIMATING      = 3;
+                          
+  var SHADE_ROLLED        = 1,
+      SHADE_UNROLLED      = 2,
+      SHADE_ANIMATING     = 3;
+                          
+  var ABSOLUTE_MIN_W      = 200,
+      ABSOLUTE_MIN_H      = 200;
+      
+  var ROLLED_HEIGHT       = (2 * hk.theme.DIALOG_PADDING) + hk.theme.DIALOG_HEADER_HEIGHT,
+      HORIZONTAL_PADDING  = (2 * hk.theme.DIALOG_PADDING),
+      VERTICAL_PADDING    = (3 * hk.theme.DIALOG_PADDING) + hk.theme.DIALOG_HEADER_HEIGHT;
+    
+  hk.DIALOG_RESIZE_NONE         = 0;  
+  hk.DIALOG_RESIZE_HORIZONTAL   = 1;
+  hk.DIALOG_RESIZE_VERTICAL     = 2;
+  hk.DIALOG_RESIZE_ANY          = 3;
+  
+  function Dialog() {
+    
+    var root = document.createElement('div');
+    root.className = 'hk-dialog';
+    
+    var header = document.createElement('header');
+    header.className = 'hk-dialog-header';
+    
+    var title = document.createElement('span');
+    title.className = 'hk-dialog-title';
+    
+    var closeButton = document.createElement('a');
+    closeButton.href = '#';
+    closeButton.className = 'hk-dialog-close';
+    
+    var shadeButton = document.createElement('a');
+    shadeButton.href = '#';
+    shadeButton.className = 'hk-dialog-shade';
+    
+    var content = document.createElement('div');
+    content.className = 'hk-dialog-content';
+    
+    var resizeHorizontal = document.createElement('div');
+    resizeHorizontal.className = 'hk-dialog-resize-handle hk-dialog-resize-horizontal';
+    
+    var resizeVertical = document.createElement('div');
+    resizeVertical.className = 'hk-dialog-resize-handle hk-dialog-resize-vertical';
+    
+    var resizeAny = document.createElement('div');
+    resizeAny.className = 'hk-dialog-resize-handle hk-dialog-resize-any';
+    
+    header.appendChild(title);
+    header.appendChild(closeButton);
+    header.appendChild(shadeButton);
+    root.appendChild(header);
+    root.appendChild(content);
+    root.appendChild(resizeHorizontal);
+    root.appendChild(resizeVertical);
+    root.appendChild(resizeAny);
+    
+    this.root = root;
+    this.header = header;
+    this.closeButton = closeButton;
+    this.shadeButton = shadeButton;
+    this.title = title;
+    this.content = content;
+    
+    //
+    // Event handlers
+    
+    this._setupDragHandler();
+    this._setupResizeHandler();
+    this._setupCloseHandler();
+    this._setupShadeHandler();
+    
+    this._visible = false;
+    this._showState = SHOW_HIDDEN;
+    hk.addClass(this.root, 'hidden hiding');
+    
+    this._rolled = false;
+    this._shadeState = SHADE_UNROLLED;
+    
+    this.setPosition(0, 0);
+    this.setMinimumSize(null, null);
+    this.setMaximumSize(null, null);
+    this.setResizePolicy(hk.DIALOG_RESIZE_ANY);
+    this.setSize(400, 400);
+    
+    this.setTitle("New Dialog");
+    
+    hk.root.appendChild(this.root);
+    
+  }
+  
+  Dialog.prototype = {
+    
+    setTitle: function(title) {
+      this.title.innerText = title;
+    },
+    
+    setResizePolicy: function(policy) {
+      this._resizePolicy = policy;
+      this._applyResizePolicy(policy);
+    },
+    
+    setPosition: function(x, y) {
+      this.x = x;
+      this.y = y;
+      this.root.style.left = x + 'px';
+      this.root.style.top = y + 'px';
+    },
+    
+    setMinimumSize: function(width, height) {
+      this.minimumWidth = width;
+      this.minimumHeight = height;
+    },
+    
+    setMaximumSize: function(width, height) {
+      this.maximumWidth = width;
+      this.maximumHeight = height;
+    },
+    
+    setSize: function(width, height) {
+      width = Math.max(ABSOLUTE_MIN_W, width);
+      if (this.minimumWidth) width = Math.max(width, this.minimumWidth);
+      if (this.maximumWidth) width = Math.min(width, this.maximumWidth);
+      
+      height = Math.max(ABSOLUTE_MIN_H, height);
+      if (this.minimumHeight) height = Math.max(height, this.minimumHeight);
+      if (this.maximumHeight) height = Math.min(height, this.maximumHeight);
+      
+      this.width = width;
+      this.height = height;
+      
+      this.content.style.width = (width - HORIZONTAL_PADDING) + 'px';
+      this.content.style.height = (height - VERTICAL_PADDING) + 'px';
+      
+      this.root.style.width = width + 'px';
+      if (!this._rolled) {
+        this.root.style.height = height + 'px';
+      }
+    },
+    
+    show: function() {
+      if (this._visible) return;
+      this._visible = true;
+      this._transitionToVisibleState();
+    },
+    
+    hide: function() {
+      if (!this._visible) return;
+      this._visible = false;
+      this._transitionToVisibleState();
+    },
+    
+    roll: function() {
+      if (this._rolled) return;
+      this._rolled = true;
+      this._applyResizePolicy(hk.DIALOG_RESIZE_NONE);
+      this.root.style.height = ROLLED_HEIGHT + 'px';
+    },
+    
+    unroll: function() {
+      if (!this._rolled) return;
+      this._rolled = false;
+      this._applyResizePolicy(this._resizePolicy);
+      this.root.style.height = this.height + 'px';
+    },
+    
+    fireClose: function() {
+      this.hide();
+    },
+    
+    fireToggleRoll: function() {
+      if (this._rolled) {
+        this.unroll();
+      } else {
+        this.roll();
+      }
+    },
+    
+    _setupDragHandler: function() {
+      var self = this;
+
+      var dragging = null;
+      this.header.addEventListener('mousedown', function(evt) {
+        dragging = [evt.pageX - self.x, evt.pageY - self.y];
+        hk.addClass(self.root, 'active-dragging');
+        hk.startCapture({
+          cursor: "move",
+          mousemove: function(evt) {
+            self.setPosition(evt.pageX - dragging[0], evt.pageY - dragging[1]);
+          },
+          mouseup: function() {
+            hk.removeClass(self.root, 'active-dragging');
+            dragging = null;
+            hk.stopCapture();
+          }
+        });
+      });
+    },
+    
+    _setupResizeHandler: function() {
+      var self = this;
+      
+      this.root.addEventListener('mousedown', function(evt) {
+        if (hk.hasClass(evt.target, 'hk-dialog-resize-handle')) {
+          
+          var initialWidth      = self.width,
+              initialHeight     = self.height,
+              initialX          = evt.pageX,
+              initialY          = evt.pageY,
+              resizeX           = true,
+              resizeY           = true,
+              cursor            = 'se-resize';
+              
+          if (evt.target.className.indexOf('-horizontal') >= 0) {
+            cursor = 'e-resize';
+            resizeY = false;
+          }
+          
+          if (evt.target.className.indexOf('-vertical') >= 0) {
+            cursor = 's-resize';
+            resizeX = false;
+          }
+          
+          hk.startCapture({
+            cursor: cursor,
+            mousemove: function(evt) {
+              var newWidth  = initialWidth + (resizeX ? (evt.pageX - initialX) : 0),
+                  newHeight = initialHeight + (resizeY ? (evt.pageY - initialY) : 0);
+              self.setSize(newWidth, newHeight);
+            },
+            mouseup: function() {
+              hk.stopCapture();
+            }
+          });
+        }
+      });
+    },
+    
+    _setupCloseHandler: function() {
+      var self = this;
+      
+      this.closeButton.addEventListener('mousedown', function(evt) {
+        evt.stopPropagation();
+      });
+      
+      this.closeButton.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        self.fireClose();
+      });
+    },
+    
+    _setupShadeHandler: function() {
+      var self = this;
+      
+      this.shadeButton.addEventListener('mousedown', function(evt) {
+        evt.stopPropagation();
+      });
+      
+      this.shadeButton.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        self.fireToggleRoll();
+      });
+    },
+    
+    _transitionToVisibleState: function() {
+      
+      if (this._showState == SHOW_ANIMATING) {
+        return;
+      }
+      
+      var self = this,
+          root = this.root;
+      
+      if (this._visible && this._showState == SHOW_HIDDEN) {
+        this._showState = SHOW_ANIMATING;
+        hk.removeClass(root, 'hidden');
+        setTimeout(function() {
+          hk.removeClass(root, 'hiding');
+          setTimeout(function() {
+            self._showState = SHOW_VISIBLE;
+            self._transitionToVisibleState();
+          }, hk.theme.DIALOG_TRANSITION_DURATION);
+        }, 0);
+      } else if (!this._visible && this._showState == SHOW_VISIBLE) {
+        this._showState = SHOW_ANIMATING;
+        hk.addClass(root, 'hiding');
+        setTimeout(function() {
+          hk.addClass(root, 'hidden');
+          self._showState = SHOW_HIDDEN;
+          self._transitionToVisibleState();
+        }, hk.theme.DIALOG_TRANSITION_DURATION);
+      }
+      
+    },
+    
+    _applyResizePolicy: function(policy) {
+      hk.removeClass(this.root, 'hk-dialog-resizable-horizontal hk-dialog-resizable-vertical hk-dialog-resizable-any');
+      
+      var resizeClass = '';
+      if (policy & hk.DIALOG_RESIZE_HORIZONTAL) resizeClass += ' hk-dialog-resizable-horizontal';
+      if (policy & hk.DIALOG_RESIZE_VERTICAL)   resizeClass += ' hk-dialog-resizable-vertical';
+      if (policy == hk.DIALOG_RESIZE_ANY)       resizeClass += ' hk-dialog-resizable-any';
+      
+      hk.addClass(this.root, resizeClass);
+    },
+  };
+  
+  hk.Dialog = Dialog;
+  
+})(this, hk);
--- hudkit/hudkit.js
+++ hudkit/hudkit.js
@@ -1,12 +1,103 @@
 ;(function(exports) {
   
+  var activeCapture = null;
+  
+  // Constants from jQuery
+  var rclass = /[\t\r\n]/g;
+  var core_rnotwhite = /\S+/g;
+  
+  function makeCaptureHandler(fn) {
+    return function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      fn(evt);
+    }
+  }
+  
   var hk = {
     root: null,
     
+    // Initialise hudkit.js with a given root element.
     init: function(root) {
-      hk.root = root;
+      hk.root = root || document.body;
       root.className = "hk";
+    },
+    
+    //
+    // Basic CSS stuff
+    
+    // from jQuery
+    hasClass: function(ele, className) {
+      className = " " + className + " ";
+      return (" " + ele.className + " ").replace(rclass, " ").indexOf(className) >= 0;
+    },
+    
+    // from jQuery
+    addClass: function(ele, value) {
+      var classes = (value || "").match(core_rnotwhite) || [],
+          cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
+      
+      if (cur) {
+        var j = 0, clazz;
+        while ((clazz = classes[j++])) {
+          if (cur.indexOf(" " + clazz + " ") < 0) {
+            cur += clazz + " ";
+          }
+        }
+        ele.className = cur.trim();
+      }
+    },
+    
+    // from jQuery
+    removeClass: function(ele, value) {
+      var classes = (value || "").match(core_rnotwhite) || [],
+          cur = ele.className ? (" " + ele.className + " ").replace(rclass, " ") : " ";
+          
+      if (cur) {
+        var j = 0, clazz;
+        while ((clazz = classes[j++])) {
+          while (cur.indexOf(" " + clazz + " ") >= 0) {
+            cur = cur.replace(" " + clazz + " ", " ");
+          }
+          ele.className = value ? cur.trim() : "";
+        }
+        
+      }
+    },
+    
+    //
+    // Event capturing
+    
+    startCapture: function(events) {
+      if (activeCapture) throw "cannot capture events, capture is already in progress";
+      activeCapture = document.createElement('div');
+      activeCapture.className = 'hk-capture-overlay';
+      document.body.appendChild(activeCapture);
+      for (var k in events) {
+        if (k == 'cursor') {
+          activeCapture.style.cursor = events[k];
+        } else {
+          activeCapture.addEventListener(k, makeCaptureHandler(events[k]));
+        }
+      }
+    },
+    
+    stopCapture: function() {
+      if (activeCapture) {
+        document.body.removeChild(activeCapture);
+        activeCapture = null;
+      }
     }
+    
+  };
+  
+  // Theme and metrics are mirrored from the SCSS file
+  // < ideal but will do for now.
+  // TODO: autogenerate this somehow
+  hk.theme = {
+    DIALOG_PADDING              : 6,
+    DIALOG_HEADER_HEIGHT        : 24,
+    DIALOG_TRANSITION_DURATION  : 200,
   };
   
   exports.hk = hk;
