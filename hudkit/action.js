f8e335221ebe165f757c3825d797288ab3cda4a0
Jason Frame <jason@onehackoranother.com>
2013-03-26 18:51:41 +0000

    rewrote action; tidier.

--- hudkit/action.js
+++ hudkit/action.js
@@ -1,51 +1,69 @@
-;(function(exports) {
+;(function(global, hk) {
   "use strict";
   
   var ActionProto = {
-    getTitle: function() {
-      return this._title;
-    },
-    isEnabled: function() {
-      return this._enabled;
-    },
-    setEnabled: function(en) {
-      this._enabled = !!en;
-    },
-    toggleEnabled: function() {
-      this.setEnabled(!this._enabled);
-    },
-    enable: function() {
-      this.setEnabled(true);
-    },
-    disable: function() {
-      this.setEnabled(false);
+    getTitle: function() { return this._title; },
+    setTitle: function(title) { this._title = title; this._notify(); },
+    
+    addObserver: function(obs) { this._observers.push(obs); },
+    removeObserver: function(obs) { hk.remove(this._observers, obs); },
+    
+    isEnabled: function() { return this._enabled; },
+    setEnabled: function(en) { this._enabled = !!en; this._notify(); },
+    toggleEnabled: function() { this.setEnabled(!this._enabled); },
+    enable: function() { this.setEnabled(true); },
+    disable: function() { this.setEnabled(false); },
+    
+    _notify: function() {
+      var os = this._observers;
+      for (var i = os.length - 1; i >= 0; --i) {
+        os[i]();
+      }
     }
   };
   
   ActionProto.__proto__ = Function.prototype;
   
-  function createAction(title, actionFun) {
-    
-    if (actionFun.__proto__ == ActionProto) {
-      return actionFun;
+  function createActionFunction(fun) {
+    if (!fun) {
+      throw "can't create action - no function given";
     }
     
-    var fun = function() {
-      if (fun._enabled) {
-        return actionFun.apply(null, arguments);
+    var actionFun = function() {
+      if (actionFun._enabled) {
+        return fun.apply(null, arguments);
       } else {
         return undefined;
       }
     }
     
-    fun._enabled  = true;
-    fun._title    = title;
-    fun.__proto__ = ActionProto;
+    actionFun._enabled    = true;
+    actionFun._title      = "";
+    actionFun._observers  = [];
+    actionFun.__proto__   = ActionProto;
     
-    return fun;
+    return actionFun;
+  }
+  
+  function createAction(spec) {
+    
+    if (spec.__proto__ == ActionProto) {
+      return spec;
+    }
+    
+    if (typeof spec == 'function') {
+      spec = { action: spec };
+    }
+    
+    var action = createActionFunction(spec.action);
+    
+    if ('title' in spec)    action._title = spec.title;
+    if ('enabled' in spec)  action._enabled = spec.enabled;
+    
+    return action;
     
   }
   
-  exports.hk.createAction = createAction;
+  hk.createAction = createAction;
   
-})(this);
+})(this, hk);
