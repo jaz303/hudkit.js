caccc3aed2f94ca4cd88459d6591b98870a9bdd7
Jason Frame <jason@onehackoranother.com>
2013-04-07 07:46:25 +0100

    Toolbar supports left/right item alignment

--- hudkit/toolbar/toolbar.js
+++ hudkit/toolbar/toolbar.js
@@ -2,7 +2,10 @@
   
   var hk = modulo.get('hk');
   
-  var TOOLBAR_ITEM_CLASS = 'hk-toolbar-item';
+  hk.TOOLBAR_ALIGN_LEFT     = 1,
+  hk.TOOLBAR_ALIGN_RIGHT    = 2;
+  
+  var TOOLBAR_ITEM_CLASS    = 'hk-toolbar-item';
   
   var superKlass = hk.Widget.prototype;
   hk.Toolbar = hk.Widget.extend({
@@ -11,7 +14,10 @@
         superKlass.init.apply(this, arguments);
       },
       
-      addAction: function(action) {
+      addAction: function(action, align) {
+        
+        align = align || hk.TOOLBAR_ALIGN_LEFT;
+        var target = (align == hk.TOOLBAR_ALIGN_LEFT) ? this._left : this._right;
         
         action = hk.createAction(action);
         
@@ -21,7 +27,7 @@
 
         hk.addClass(actionButton.getRoot(), TOOLBAR_ITEM_CLASS);
         
-        this._attachChildViaElement(actionButton, this.root);
+        this._attachChildViaElement(actionButton, target);
         
         return action;
         
@@ -33,6 +39,15 @@
       
       _buildStructure: function() {
         this.root = document.createElement('div');
+        
+        this._left = document.createElement('div');
+        this._left.className = 'hk-toolbar-items hk-toolbar-items-left';
+        this.root.appendChild(this._left);
+        
+        this._right = document.createElement('div');
+        this._right.className = 'hk-toolbar-items hk-toolbar-items-right';
+        this.root.appendChild(this._right);
+        
         this.root.className = 'hk-toolbar';
       }
     }
